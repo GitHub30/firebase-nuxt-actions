eba9b8360643b79b3d71bae9398b4c3482877862
Tomofumi Inoue <funaox@gmail.com>
2019-08-20 22:21:36 +0900

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        firebase-nuxt-actions
+        firebase-nuxt-actions2
       </h1>
       <h2 class="subtitle">
         My superior Nuxt.js project
